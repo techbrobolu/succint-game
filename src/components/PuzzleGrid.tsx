cdac6d39cd8279cb77ec3224672baecf9933b2b5
techbrobolu <techbrobolu@gmail.com>
2025-04-29 08:48:11 +0100

    Fix puzzle image

--- src/components/PuzzleGrid.tsx
+++ src/components/PuzzleGrid.tsx
@@ -14,7 +14,7 @@ const PuzzleGrid: React.FC<PuzzleGridProps> = ({ size }) => {
     return (
       <div className="w-full h-full">
         <img 
-          src="../public/logo.png" 
+          src="../public/Succinct.png" 
           alt="Complete Puzzle" 
           className="w-full h-full object-cover"
         />
